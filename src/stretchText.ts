cebe7832f2b0a19adf18a13fc10041283368f081
Dominik Dewitz <dewitzdominik@kabelmail.de>
2022-01-11 11:32:22 +0100

    Refactor Text Stretching

--- src/stretchText.ts
+++ src/stretchText.ts
@@ -6,22 +6,10 @@ const stretchText = () => {
   );
 
   elements.forEach((element) => {
-    const width = element.clientWidth;
-    const charNum = element.textContent ? element.textContent.length : 0;
-    const spacing = width / charNum;
-    const text = element.textContent;
-
-    const stretchSpan = document.createElement("span");
-    stretchSpan.classList.add("stretch-span");
-    stretchSpan.textContent = text;
-    element.innerHTML = "";
-    element.appendChild(stretchSpan);
-
-    const stretchSpanWidth = stretchSpan.getBoundingClientRect().width;
-    const charWidth = stretchSpanWidth / charNum;
-    const letterSpacing = spacing - charWidth + (spacing - charWidth) / charNum;
-
-    stretchSpan.style.letterSpacing = `${letterSpacing + 3.5}px`;
+    const wordsArray = element.innerText
+      .split("")
+      .map((e) => (" " == e ? "<div>&nbsp</div>" : "<div>" + e + "</div>"));
+    element.innerHTML = wordsArray.join("");
   });
 };
 
