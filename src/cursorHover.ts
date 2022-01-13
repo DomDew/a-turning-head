5c367a65ac891e0df29ce3a4ab9fc81547bea72b
Dominik Dewitz <dewitzdominik@kabelmail.de>
2022-01-13 21:51:36 +0100

    Remove Cursor on Touch Devices

--- src/cursorHover.ts
+++ src/cursorHover.ts
@@ -1,6 +1,10 @@
+import isTouchDevice from "./checkTouchDevice";
+
 const cursorHover = () => {
   const hoverElements = document.querySelectorAll(".hoverable");
   const cursor = document.getElementById("cursor") as HTMLElement;
+
+  isTouchDevice && (cursor.style.display = "none");
 
   hoverElements.forEach((element) => {
     element.addEventListener("mouseover", () => {
